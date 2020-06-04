1cbd1059b7fd1db7a26c3d631e50939dfcf0999f
jeancarlos0 <jecarlos.me@gmail.com>
2020-06-03 22:36:10 -0300

    Implemented select items

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -30,11 +30,14 @@ function getCities(event){
     var ufValue = event.target.value;
     var url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
     
+    citySelect.innerHTML = '<option value>Selecione uma cidade</option>';
+    citySelect.disabled = true;
+
     fetch(url)
     .then(res =>  res.json())
     .then(cities => {
         for(city of cities){
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
         }
     })
 
@@ -44,3 +47,64 @@ function getCities(event){
 }
 
 document.querySelector('select[name = uf]').addEventListener('change', getCities);
+
+//Itens de coleta
+
+const itensToColet = document.querySelectorAll('.items-grid li');
+
+for(item of itensToColet){
+    item.addEventListener('click', handleSelectedItem);
+}
+
+
+const collectedItems = document.querySelector('input[name=items]');
+
+let selectedItems = [];
+
+function handleSelectedItem(event){
+
+    const itemLi = event.target;
+    const itemId = itemLi.dataset.id;
+    
+    itemLi.classList.toggle('selected');
+
+    //1 - Verificar se existem items selecionados
+
+    const alreadySelected = selectedItems.findIndex(item => {
+        //retorna o resultado da operação, true ou false
+        return item == itemId;
+    })
+    // 3 - Se já estiver selecionado tirar da seleção
+    if(alreadySelected != -1){
+        //A função anônima é executada para cada item do array, e retorna true ou falso
+        //A função vai verificar se o item é diferente dos items presentes no array, se for, ele
+        //será incluso no novo array
+        const filteredItems = selectedItems.filter(item => {
+            const itemIsDifferent = item != itemId;
+            return itemIsDifferent;
+        })
+        selectedItems = filteredItems;
+    }else{
+        //4 - Senão, adicionar a seleção
+        selectedItems.push(itemId);
+    }
+
+    
+    //2 - Se sim, pegar os items 5 - Att o hidden input com o item selecionado
+    collectedItems.value = selectedItems;
+   
+
+    
+
+    /*
+
+    const alreadySelected = selectedItems.findIndex(function(item){
+        const itemFound  = item == itemId;
+        return itemFound
+    })
+
+    const alreadySelected = selectedItems.findIndex(item => item == itemId)
+    */
+
+
+}
